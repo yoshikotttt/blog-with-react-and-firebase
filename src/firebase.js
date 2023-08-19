cf57f80659fed40cff49a43b17186a9af3389cc0
yoshikotttt <myqic.n@gmail.com>
2023-08-19 14:41:06 +0900

    firebaseConfig　変更

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,28 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseError, initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider} from "firebase/auth";
 import { getFirestore} from "firebase/firestore";
 
 
-
-const app = initializeApp({
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+// const app = initializeApp({
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID
+// })
 
 // Firebase の各サービスへの接続と初期化を行い、他のファイルでそれらのインスタンスを再利用できるようにする
 // const app = initializeApp(firebaseConfig);
@@ -22,4 +33,4 @@ const provider = new GoogleAuthProvider();
 
 const db = getFirestore(app);
 
-export {app, auth, provider, db};
+export {auth, provider, db};
